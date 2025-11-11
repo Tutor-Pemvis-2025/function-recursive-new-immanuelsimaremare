5c91b97a259677b61c63de18418e645e17a0ccd4
immanuelsimaremare-max <immanuelsimaremare@gmail.com>
2025-11-11 20:54:17 +0700

    TUTOR
    selesai

---
+++ TFUNC.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.Math;
+
+public hitungRekursif {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int angka, hasil;
+
+        angka = Integer.parseInt(input.nextLine());
+        hasil = hitungRekursif(angka);
+    }
+    
+    public static int hitungRekursif(int angka) {
+        int hasil;
+
+        if (angka == 1) {
+            hasil = 1;
+        } else {
+            hasil = angka + hitungRekursif(angka - 1);
+        }
+        
+        return hasil;
+    }
+}
